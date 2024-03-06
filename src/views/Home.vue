3d9f415f73aa2a987454a2dbea527edf61e409e7
GuilhermeSchwengber20 <guilhermenft2021@gmail.com>
2024-03-06 11:34:01 -0300

    adicionando configurações de eslint e prettier

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,37 +1,36 @@
 <script lang="ts" setup>
-import { onMounted, ref } from "vue";
-import { useStore } from "vuex";
-import CardHeaderHome from "@/components/CardHeaderHome.vue";
-// import PostCard from "@/components/PostCard.vue";
+  import { onMounted, ref } from 'vue'
+  import { useStore } from 'vuex'
+  import CardHeaderHome from '@/components/CardHeaderHome.vue'
+  // import PostCard from "@/components/PostCard.vue";
 
-import { useRouter } from "vue-router";
+  import { useRouter } from 'vue-router'
 
-const router = useRouter();
-const store = useStore();
-const Posts = ref([]);
+  const router = useRouter()
+  const store = useStore()
+  const Posts = ref([])
 
-onMounted(async () => {
-  await store.dispatch("fetchPosts") 
-  Posts.value = store.state.posts
-});
+  onMounted(async () => {
+    await store.dispatch('fetchPosts')
+    Posts.value = store.state.posts
+  })
 
+  // Função para obter o caminho da imagem
+  const getImageSrc = (name: string): string => {
+    return new URL(name, import.meta.url).href
+  }
 
-// Função para obter o caminho da imagem
-const getImageSrc = (name: string): string => {
-  return new URL(name, import.meta.url).href;
-};
+  // Função para determinar a classe da grade
+  const getGridClass = (index: number): string => {
+    return (index + 1) % 6 === 1 || (index + 1) % 6 === 0
+      ? 'col-span-2'
+      : 'col-span-1'
+  }
 
-// Função para determinar a classe da grade
-const getGridClass = (index: number): string => {
-  return (index + 1) % 6 === 1 || (index + 1) % 6 === 0
-    ? "col-span-2"
-    : "col-span-1";
-};
-
-const navigateToPostDetails = ( index : number) => {
-  console.log(`Navigating to PostDetails with index: ${index}`);
-  router.push({ name: "PostDetails", params: {index: index}});
-}
+  const navigateToPostDetails = (index: number) => {
+    console.log(`Navigating to PostDetails with index: ${index}`)
+    router.push({ name: 'PostDetails', params: { index: index } })
+  }
 </script>
 
 <template>
@@ -45,34 +44,34 @@ const navigateToPostDetails = ( index : number) => {
         v-for="(post, index) in Posts"
         :key="index"
         :class="`${getGridClass(
-          index
+          index,
         )} group relative flex lg:h-[600px] items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg`"
         @click="navigateToPostDetails(index)"
       >
-          <img
-            :src="getImageSrc(post.urlToImage)"
-            class="absolute inset-0 h-full w-full object-cover object-center transition duration-150 ease-in group-hover:scale-110"
-            loading="lazy"
-          />
-          <div
-            class="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50"
-          ></div>
+        <img
+          :src="getImageSrc(post.urlToImage)"
+          class="absolute inset-0 h-full w-full object-cover object-center transition duration-150 ease-in group-hover:scale-110"
+          loading="lazy"
+        />
+        <div
+          class="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50"
+        ></div>
 
-          <div
-            class="flex flex-col lg:ml-4 lg:mb-6 justify-start items-start relative lg:gap-2"
+        <div
+          class="flex flex-col lg:ml-4 lg:mb-6 justify-start items-start relative lg:gap-2"
+        >
+          <span
+            class="inline-block text-lg font-primary uppercase font-medium text-white"
           >
-            <span
-              class="inline-block text-lg font-primary uppercase font-medium text-white"
-            >
-              {{ post.title }}
-            </span>
-            <span
-              class="inline-block text-sm uppercase font-secondary text-white"
-            >
-              {{ post.description }}
-            </span>
-          </div>
+            {{ post.title }}
+          </span>
+          <span
+            class="inline-block text-sm uppercase font-secondary text-white"
+          >
+            {{ post.description }}
+          </span>
+        </div>
       </span>
     </div>
-</div>
+  </div>
 </template>
--- src/views/PostDetails.vue
+++ src/views/PostDetails.vue
@@ -1,58 +1,61 @@
 <template>
-    <div class="bg-gradient-to-b from-black to-background lg:h-auto lg:w-full lg:pl-24 lg:pr-8 lg:py-5 flex flex-col">
-        <img :src="post.urlToImage" class="w-full h-96 rounded-md object-cover" loading="lazy"/>
-        <div class="flex flex-col justify-start">
-            <h2 class="font-primary text-2xl uppercase font-bold text-white lg:py-6 lg:px-32">
-                {{ post.title }}
-            </h2>
-            <div class="flex lg:gap-10 lg:px-10">
-                <div 
-                    @click="backToHome"
-                    class="cursor-pointer lg:min-w-12 lg:min-h-12 lg:max-w-12 lg:max-h-12 rounded-full text-center bg-primary flex justify-center items-center "
-                >
-                    <ArrowLeftIcon  />
-                </div>
-                <p class="text-[#7C7C7C] font-secondary font-normal lg:mx-2 text-justify">
-                    {{ post.content }}  
-                </p>
-
-            </div>
+  <div
+    class="bg-gradient-to-b from-black to-background lg:h-auto lg:w-full lg:pl-24 lg:pr-8 lg:py-5 flex flex-col"
+  >
+    <img
+      :src="post.urlToImage"
+      class="w-full h-96 rounded-md object-cover"
+      loading="lazy"
+    />
+    <div class="flex flex-col justify-start">
+      <h2
+        class="font-primary text-2xl uppercase font-bold text-white lg:py-6 lg:px-32"
+      >
+        {{ post.title }}
+      </h2>
+      <div class="flex lg:gap-10 lg:px-10">
+        <div
+          class="cursor-pointer lg:min-w-12 lg:min-h-12 lg:max-w-12 lg:max-h-12 rounded-full text-center bg-primary flex justify-center items-center"
+          @click="backToHome"
+        >
+          <ArrowLeftIcon />
         </div>
-        
+        <p
+          class="text-[#7C7C7C] font-secondary font-normal lg:mx-2 text-justify"
+        >
+          {{ post.content }}
+        </p>
+      </div>
     </div>
+  </div>
 </template>
 
-
-
 <script setup lang="ts">
-import ArrowLeftIcon from "@/components/icons/ArrowLeftIcon.vue";
-import { ref, onMounted } from "vue";
+  import ArrowLeftIcon from '@/components/icons/ArrowLeftIcon.vue'
+  import { ref, onMounted } from 'vue'
 
-import { useStore } from "vuex";
-import { useRoute, useRouter } from "vue-router";
+  import { useStore } from 'vuex'
+  import { useRoute, useRouter } from 'vue-router'
 
+  const store = useStore()
+  const route = useRoute()
+  const router = useRouter()
 
-const store = useStore();
-const route = useRoute();
-const router = useRouter();
+  const post = ref({})
 
-const post = ref({});
+  onMounted(() => {
+    const index: Number = route.params.index
 
-onMounted(() => {
-    const index : Number = route.params.index;
+    console.log(index)
 
-    console.log(index);
+    const posts = store.state.posts
 
-    const posts = store.state.posts;
-
-    if(index >= 0 && index < posts.length) {
-        post.value = posts[index];
+    if (index >= 0 && index < posts.length) {
+      post.value = posts[index]
     }
-})
+  })
 
-
-
-const backToHome = () => {
-    router.go(-1);
-}
+  const backToHome = () => {
+    router.go(-1)
+  }
 </script>
